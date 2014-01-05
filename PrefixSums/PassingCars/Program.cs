4f92b58dcdcc02c666f3ce923a6a62890638d96c
Darren Seet Serkai <darren@seet.me>
2014-01-06 00:01:10 +0900

    Added PassingCars

---
+++ PrefixSums/PassingCars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().solution(new [] { 0, 1, 0, 1, 1 }));
+        }
+
+        public int solution(int[] A)
+        {
+            // write your code in C# with .NET 2.0
+            var length = A.Length;
+            if (length < 0 || length > 1000000) throw new ArgumentOutOfRangeException();
+
+            var eastBoundCars = 0;
+            var passingPairs = 0;
+
+            foreach (var value in A)
+            {
+                if (value == 1)
+                {
+                    passingPairs += eastBoundCars;
+                    if (passingPairs > 1000000000) return -1;
+                }
+                else
+                    eastBoundCars++;
+            }
+            return passingPairs;
+        }
+    }
+}
