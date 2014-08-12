49fed51c7271d7534eb83b14ab066f94d8f4b346
oredi <darren.seet@gmail.com>
2014-08-12 10:19:27 +0900

    Update Program.cs

--- CountingElements/FrogRiverOne/Program.cs
+++ CountingElements/FrogRiverOne/Program.cs
@@ -1,7 +1,10 @@
 /*
-A small frog wants to get to the other side of a river. The frog is currently located at position 0, and wants to get to position X. Leaves fall from a tree onto the surface of the river.
-You are given a non-empty zero-indexed array A consisting of N integers representing the falling leaves. A[K] represents the position where one leaf falls at time K, measured in minutes.
-The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only when leaves appear at every position across the river from 1 to X.
+A small frog wants to get to the other side of a river. The frog is currently located at position 0, and wants to get 
+to position X. Leaves fall from a tree onto the surface of the river.
+You are given a non-empty zero-indexed array A consisting of N integers representing the falling leaves. A[K] represents
+the position where one leaf falls at time K, measured in minutes.
+The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only 
+when leaves appear at every position across the river from 1 to X.
 For example, you are given integer X = 5 and array A such that:
   A[0] = 1
   A[1] = 3
@@ -11,10 +14,12 @@ For example, you are given integer X = 5 and array A such that:
   A[5] = 3
   A[6] = 5
   A[7] = 4
-In minute 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the river.
+In minute 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the 
+river.
 Write a function:
 int solution(int X, int A[], int N);
-that, given a non-empty zero-indexed array A consisting of N integers and integer X, returns the earliest time when the frog can jump to the other side of the river.
+that, given a non-empty zero-indexed array A consisting of N integers and integer X, returns the earliest time when 
+the frog can jump to the other side of the river.
 If the frog is never able to jump to the other side of the river, the function should return −1.
 For example, given X = 5 and array A such that:
   A[0] = 1
@@ -30,7 +35,8 @@ N and X are integers within the range [1..100,000];
 each element of array A is an integer within the range [1..X].
 Complexity:
 expected worst-case time complexity is O(N);
-expected worst-case space complexity is O(X), beyond input storage (not counting the storage required for input arguments).
+expected worst-case space complexity is O(X), beyond input storage (not counting the storage required for input 
+arguments).
 Elements of input arrays can be modified.
 */
 using System;
