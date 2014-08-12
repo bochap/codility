108379ae076bcf73c71f7ff4a1005e72c7d79699
oredi <darren.seet@gmail.com>
2014-08-12 10:11:20 +0900

    Update Program.cs

--- TimeComplexity/FrogJmp/Program.cs
+++ TimeComplexity/FrogJmp/Program.cs
@@ -1,9 +1,11 @@
 /*
-A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to 
+a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
 Count the minimal number of jumps that the small frog must perform to reach its target.
 Write a function:
 int solution(int X, int Y, int D);
-that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to 
+or greater than Y.
 For example, given:
   X = 10
   Y = 85
